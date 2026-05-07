f7f4b5bbf4b43af9835ffe1308529df4a5bbe263
Bellacimax <partecipazionicd@gmail.com>
2026-05-07 14:56:04 +0200

    stats.py

--- stats.py
+++ stats.py
@@ -1,1 +1,72 @@
+import csv
+import os
+from datetime import datetime
+
+FILE_NAME = "trade_history.csv"
+
+# =========================================
+# CREA FILE
+# =========================================
+
+def init_stats():
+
+    if not os.path.exists(FILE_NAME):
+
+        with open(FILE_NAME, "w", newline="") as f:
+
+            writer = csv.writer(f)
+
+            writer.writerow([
+
+                "time",
+                "ticker",
+                "side",
+                "entry",
+                "exit",
+                "pnl",
+                "rr",
+                "result"
+
+            ])
+
+# =========================================
+# SALVA TRADE
+# =========================================
+
+def save_trade(
+
+    ticker,
+    side,
+    entry,
+    exit_price,
+    pnl,
+    rr,
+    result
+
+):
+
+    with open(FILE_NAME, "a", newline="") as f:
+
+        writer = csv.writer(f)
+
+        writer.writerow([
+
+            datetime.now(),
+
+            ticker,
+
+            side,
+
+            round(entry, 2),
+
+            round(exit_price, 2),
+
+            round(pnl, 2),
+
+            rr,
+
+            result
+
+        ])
+
 
