26193d9dc83f02034fd16397e2833e6381a9b195
Bellacimax <partecipazionicd@gmail.com>
2026-05-12 15:45:12 +0200

    main.py

--- main.py
+++ main.py
@@ -613,25 +613,25 @@ while True:
 
             if df is None or df.empty:
 
-            print(f"❌ NO DATA -> {ticker}")
+                print(f"❌ NO DATA -> {ticker}")
 
                 continue
 
-             if len(df) < 50:
+            if len(df) < 50:
 
-                 continue
+                continue
 
-         if isinstance(df.columns, pd.MultiIndex):
+            if isinstance(df.columns, pd.MultiIndex):
 
-         df.columns = df.columns.get_level_values(0)
+                df.columns = df.columns.get_level_values(0)
 
-         df = df[[
-            "Open",
-            "High",
-            "Low",
-            "Close",
-            "Volume"
-          ]].dropna()
+            df = df[[
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Volume"
+            ]].dropna()
             
 
             # =========================================
