d7a6c83e7b45efd1c045e401b88e58ae3bb65e6e
Bellacimax <partecipazionicd@gmail.com>
2026-05-13 14:50:55 +0200

    main.py

--- main.py
+++ main.py
@@ -1009,6 +1009,17 @@ while True:
             # =========================================
 
             if side == "SELL" and rsi < 10:
+            if side == "BUY" and price < vwap:
+
+                print(f"⚠️ BELOW VWAP -> {ticker}")
+
+                continue
+
+            if side == "SELL" and price > vwap:
+
+                print(f"⚠️ ABOVE VWAP -> {ticker}")
+
+                continue
 
                 print(f"⚠️ RSI TOO LOW -> {ticker}")
 
