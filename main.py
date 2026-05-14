d0079bd5c0fb0f06ab6e9a32274891b3af02ba81
Bellacimax <partecipazionicd@gmail.com>
2026-05-14 10:38:49 +0200

    main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import csv
 from datetime import datetime, UTC
 from scanner import rank_tickers
 from datetime import timedelta
-
-
+            
 # =========================================
 # TELEGRAM
 # =========================================
