5d5ff5a231cd335ef72358a06569b2c2a096ca31
Bellacimax <partecipazionicd@gmail.com>
2026-05-27 15:38:02 +0200

    main.py

--- main.py
+++ main.py
@@ -574,29 +574,34 @@ def trading_loop():
 
                 continue
 
-# =========================================
-# LOOP TICKER
-# =========================================
 
-time.sleep(3)
-
-for ticker in subset:
+while True:
 
     try:
 
-        print(f"🔍 Analizzo {ticker}")
+        # =========================================
+        # LOOP TICKER
+        # =========================================
+
+        time.sleep(3)
+
+        for ticker in subset:
+
+            try:
+
+                print(f"🔍 Analizzo {ticker}")
+
+            except Exception as e:
+
+                print(f"❌ ERRORE {ticker}: {e}")
+
+                continue
 
     except Exception as e:
 
-        print(f"❌ ERRORE {ticker}: {e}")
+        print(f"❌ ERRORE LOOP: {e}")
 
-        continue
-
-except Exception as e:
-
-    print(f"❌ ERRORE LOOP: {e}")
-
-    time.sleep(60)
+        time.sleep(60)
 
             # =========================================
             # WEEKEND FILTER
